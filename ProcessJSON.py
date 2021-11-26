998d5c21a9d10429269b43cae0e87ee899783a26
Muhammad Hassan <50042969+evilgenius786@users.noreply.github.com>
2021-11-27 00:23:54 +0500

    Calculation fixed

--- ProcessJSON.py
+++ ProcessJSON.py
@@ -3,31 +3,143 @@ import os
 import re
 import traceback
 
-mapteam = {}
+try:
+    with open('map.json') as mfile:
+        mapteam = json.load(mfile)
+except:
+    mapteam = {}
+
+
+
+def main():
+    # createMap()
+    # convertOdds()
+    # processFiles()
+    processOdds()
+
+
+def processOdds():
+    result = []
+    with open("highest.json") as hfile:
+        highest = json.load(hfile)
+    with open("mapteam.json") as mfile:
+        mapteam = json.load(mfile)
+    for team in mapteam.keys():
+        for smt in highest.keys():
+            try:
+                odd1 = float(highest[smt][team]['Value'].split()[-1])
+                odd2 = float(highest[smt][mapteam[team]]['Value'].split()[-1])
+                # print("Odd1", odd1, "Odd2", odd2)
+                if odd1 != 0 and odd2 != 0:
+                    arb = (100 / odd1) + (100 / odd2)
+                    data = {
+                        "Over": team,
+                        "Under": mapteam[team],
+                        "Line": smt,
+                        "OverInfo": highest[smt][team],
+                        "UnderInfo": highest[smt][mapteam[team]],
+                        "Arb": round(arb, 2),
+                        "Investment": 100,
+                        "Profit": round((100 / arb) - 100, 2)
+                    }
+                    print(json.dumps(data, indent=4))
+                    if data['Arb'] < 100:
+                        result.append(data)
+            except KeyError:
+                # traceback.print_exc()
+                # input()
+                pass
+    with open("result.json", "w") as rfile:
+        json.dump(result, rfile, indent=4)
+
+def processFiles():
+    data = {}
+    for directory in os.listdir('./'):
+        if os.path.isdir(directory) and not directory.startswith('.'):
+            teams = {}
+            games = {}
+            for file in os.listdir(directory):
+                if file.startswith("-"):
+                    with open(f'./{directory}/{file}') as jfile:
+                        js = json.load(jfile)
+                        f = file[1:-5]
+                        games[f] = {}
+                        for league in js:
+                            team1 = league["Team1"]['Name']
+                            team2 = league["Team2"]['Name']
+                            if team1 not in teams.keys():
+                                teams[team1] = team2
+                            del league['Team1']['Name']
+                            del league['Team2']['Name']
+                            games[f][team1] = league['Team1']
+                            games[f][team2] = league['Team2']
+            # print("============================================")
+            # print(json.dumps(teams, indent=4))
+            # print("============================================")
+            # print(json.dumps(games, indent=4))
+
+            for smt in ["Spread", "Money", "Total"]:
+                if smt not in data.keys():
+                    data[smt] = {}
+                for team in teams.keys():
+                    # if team not in data[smt].keys():
+                    data[smt][team] = {"Value": "0", "Service": "None"}
+                    # break
+                    # if teams[team] not in data[smt].keys():
+                    data[smt][teams[team]] = {"Value": "0", "Service": "None"}
+                    # break
+            # print("=============================================")
+            # print("Data: ", json.dumps(data, indent=4))
+
+            for service in games.keys():
+                for team in teams.keys():
+                    for smt in ["Spread", "Money", "Total"]:
+                        if team in games[service].keys():
+                            # print(data[smt][team]['Value'])
+                            # print(team, games[service][team][smt])
+                            if data[smt][teams[team]]['Service'] != service and float(
+                                    data[smt][team]['Value'].split()[-1]) < float(
+                                    games[service][team][smt].split()[-1]):
+                                data[smt][team] = {"Value": games[service][team][smt], "Service": service}
+                        if teams[team] in games[service].keys():
+                            if data[smt][team]['Service'] != service and float(
+                                    data[smt][teams[team]]['Value'].split()[-1]) < float(
+                                    games[service][teams[team]][smt].split()[-1]):
+                                data[smt][teams[team]] = {"Value": games[service][teams[team]][smt], "Service": service}
+            # print("=============================================")
+    # print(json.dumps(data, indent=4))
+    with open("highest.json", 'w') as hfile:
+        json.dump(data, hfile, indent=4)
 
 
 def createMap():
     teams = []
+    teammap = {}
     for directory in os.listdir():
         if os.path.isdir(directory) and not directory.startswith('.'):
             for file in os.listdir(directory):
-                with open(f"./{directory}/{file}") as infile:
-                    js = json.load(infile)
-                    try:
-                        for league in js[[k for k in js.keys()][0]]:
-                            try:
-                                for team in league.keys():
-                                    if team not in teams:
-                                        teams.append(team)
-                            except:
-                                traceback.print_exc()
-                                pass
-                    except:
-                        pass
-                        traceback.print_exc()
+                if not file.startswith("-"):
+                    with open(f"./{directory}/{file}") as infile:
+                        js = json.load(infile)
+                        try:
+                            leagues = js[[k for k in js.keys()][0]]
+                            for league in leagues:
+                                try:
+                                    for team in league.keys():
+                                        lks = [k for k in league.keys()]
+                                        teammap[lks[0]] = lks[1]
+                                        if team not in teams:
+                                            teams.append(team)
+                                except:
+                                    traceback.print_exc()
+                                    pass
+                        except:
+                            pass
+                            traceback.print_exc()
     teams.sort()
     temp = {}
-    # print(teams)
+    with open("mapteam.json", 'w') as hfile:
+        json.dump(teammap, hfile, indent=4)
     for team in teams:
         rotnum = team.split()[0]
         if rotnum.isdigit() and "(" not in team and len(team.split()) > 2:
@@ -38,20 +150,17 @@ def createMap():
         if rotnum.isdigit():
             if rotnum not in temp.keys() or len(temp[rotnum]) < len(team):
                 temp[rotnum] = team.title()
-    # for team in teams:
-    #     if team not in temp.keys():
-    #         temp[team] = team.title()
-    print(json.dumps(temp, indent=4))
-    with open("teams.json", 'w') as tfile:
-        json.dump(temp, tfile, indent=4)
+    # print(json.dumps(temp, indent=4))
+
     for team in teams:
         for k in temp.keys():
             try:
-                if team.split(" ")[0] == temp[k].split(" ")[0] or team.lower() in temp[k].lower():
+                if team not in mapteam.keys() and team.split(" ")[0] == temp[k].split(" ")[0] or team.lower() in temp[
+                    k].lower():
                     mapteam[team] = temp[k]
                     break
             except:
-                pass
+                traceback.print_exc()
     for team in teams:
         for k in temp.keys():
             try:
@@ -60,37 +169,24 @@ def createMap():
                     if word in temp[k].split(" "):
                         found += 1
                 if found > 1:
-                    mapteam[team] = temp[k]
-                    break
+                    if team not in mapteam.keys():
+                        mapteam[team] = temp[k]
+                        break
             except:
-                pass
-    # for team in teams:
-    #     for k in temp.keys():
-    #         try:
-    #             found = 0
-    #             for word in team.split(" "):
-    #                 if word in temp[k].split(" "):
-    #                     found += 1
-    #             if found > 0:
-    #                 mapteam[team] = temp[k]
-    #                 break
-    #         except:
-    #             pass
-    print(json.dumps(mapteam, indent=4))
-    with open("map.json", 'w') as mfile:
-        json.dump(mapteam, mfile, indent=4)
+                traceback.print_exc()
     for team in teams:
         if team not in mapteam.keys():
-            print(team)
-    print("Map", len([k for k in mapteam.keys()]))
-    print("Temp", len([k for k in temp.keys()]))
-    print("teams", len(teams))
-    print("Teams left", len(teams) - len([k for k in mapteam.keys()]))
-
-
-def main():
-    createMap()
-    convertOdds()
+            mapteam[team] = team
+    # print(json.dumps(mapteam, indent=4))
+    with open("map.json", 'w') as mfile:
+        json.dump(mapteam, mfile, indent=4)
+    # for team in teams:
+    #     if team not in mapteam.keys():
+    #         print(team)
+    # print("Map", len([k for k in mapteam.keys()]))
+    # print("Temp", len([k for k in temp.keys()]))
+    # print("teams", len(teams))
+    # print("Teams left", len(teams) - len([k for k in mapteam.keys()]))
 
 
 def convertOdds():
@@ -109,15 +205,17 @@ def convertOdds():
                             team2 = league[lkeys[1]]
                             try:
                                 newleague = {
-                                    mapteam[lkeys[0]]: {
-                                        "Spread": getres(team1["Spread"]),
-                                        "Money": getres(team1["Money"]),
-                                        "Total": getres(team1["Total"])
+                                    "Team1": {
+                                        "Name": mapteam[lkeys[0]],
+                                        "Spread": getres(team1["Spread"] if "Spread" in team1.keys() else "0 0"),
+                                        "Money": getres(team1["Money"] if "Money" in team1.keys() else "0"),
+                                        "Total": getres(team1["Total"] if "Total" in team1.keys() else "0 0")
                                     },
-                                    mapteam[lkeys[1]]: {
-                                        "Spread": getres(team2["Spread"]),
-                                        "Money": getres(team2["Money"]),
-                                        "Total": getres(team2["Total"])
+                                    "Team2": {
+                                        "Name": mapteam[lkeys[1]],
+                                        "Spread": getres(team2["Spread"] if "Spread" in team2.keys() else "0 0"),
+                                        "Money": getres(team2["Money"] if "Money" in team2.keys() else "0"),
+                                        "Total": getres(team2["Total"] if "Total" in team2.keys() else "0 0")
                                     },
                                 }
                                 newjs.append(newleague)
@@ -127,10 +225,9 @@ def convertOdds():
                                 traceback.print_exc()
                                 # input("Press any key")
                             # break
-                    print(json.dumps(newjs, indent=4))
+                    # print(json.dumps(newjs, indent=4))
                     with open(f"./{directory}/-{file}", 'w') as jfile:
                         json.dump(newjs, jfile, indent=4)
-            # break
 
 
 def getres(res=""):
@@ -140,11 +237,13 @@ def getres(res=""):
         if odd > 10:
             odd = 1 + (odd / 100)
         elif odd < -10:
-            odd = 1 - (odd / 100)
+            odd = 1 - (100 / odd)
         if len(r) == 1:
-            res = f"{odd}"
+            res = f"{round(odd, 2)}"
         elif len(r) == 2:
-            res = f"{r[0]} {odd}"
+            res = f"{r[0]} {round(odd, 2)}"
+    else:
+        res = "0"
     return res
 
 
