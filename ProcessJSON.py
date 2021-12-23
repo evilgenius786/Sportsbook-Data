b03b5e2646eba9b4b31fd16b09f3368a25f30ca9
Muhammad Hassan <50042969+evilgenius786@users.noreply.github.com>
2021-12-23 19:32:54 +0500

    Calculation fixed

--- ProcessJSON.py
+++ ProcessJSON.py
@@ -1,7 +1,24 @@
+import datetime
 import json
 import os
 import re
+import threading
+import time
 import traceback
+
+from flask import Flask, send_from_directory
+
+import betdsi
+import betnow
+import betus
+import gtbets
+import intertops
+import jazzsports
+import mybookie
+import xbet
+import youwager
+
+app = Flask(__name__)
 
 try:
     with open('map.json') as mfile:
@@ -10,12 +27,79 @@ except:
     mapteam = {}
 
 
+@app.route('/', defaults=dict(filename=None))
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def index(filename):
+    filename = filename or 'index.html'
+    # if request.method == 'GET':
+    return send_from_directory('.', filename)
+
+
+def createHtml():
+    html = f"""<html>
+    <head>
+        <title>Sportsbook Data - BetDSI, BetNow, BetUS, GTbets, InterTops, JazzSports, MyBookie, xBet, YouWager  </title>
+        <link rel="icon" type="image/x-icon" href="/sports.png">
+        <b>Last updated: {datetime.datetime.now()}</b>
+        <style>
+            td {{text-align: center;}}
+            table, th, td {{border: 1px solid black}}
+            th, td {{padding: 2px}}
+        </style>
+    </head>
+    <body>
+    <table>
+        <tr>
+            <th>Teams</th>
+            <th>Over</th>
+            <th>Under</th>
+        </tr>"""
+    with open('result.json') as rfile:
+        for result in json.load(rfile):
+            html += f"""<tr>
+            <td>{result['Over']}</td>
+            <td>{result['OverInfo']['Value']} - O</td>
+            <td>{result['UnderInfo']['Value']} - U</td>
+        </tr>
+        <tr>
+            <td>{result['Under']}</td>
+            <td><img width="100px" src="./.img/{result['OverInfo']['Service']}.png"></td>
+            <td><img width="100px" src="./.img/{result['UnderInfo']['Service']}.png"></td>
+        </tr>
+        <tr>
+            <td colspan="3">Sport: <b>{result['OverInfo']['Sport']}</b>\tLine: <b>{result['Line']}</b>\tArb: <b>{result['Arb']}</b>\tInv: <b>{result['Investment']}</b>\tProf: <b>{result['Profit']}</b></td>
+        </tr>
+        <tr style="height: 20px"></tr>"""
+        html += """</table>
+</body>
+</html>"""
+    with open("index.html", 'w') as rfile:
+        rfile.write(html)
+
 
 def main():
-    # createMap()
-    # convertOdds()
-    # processFiles()
+    # getFreshData()
+    print(datetime.datetime.now(), "Processing files..")
+    createMap()
+    convertOdds()
+    processFiles()
     processOdds()
+    print(datetime.datetime.now(), "Processing files finished!")
+    createHtml()
+
+
+def getFreshData():
+    print(datetime.datetime.now(), "Fetching live data...")
+    threads = [threading.Thread(target=betus.main), threading.Thread(target=betdsi.main),
+               threading.Thread(target=betnow.main), threading.Thread(target=gtbets.main),
+               threading.Thread(target=intertops.main), threading.Thread(target=jazzsports.main),
+               threading.Thread(target=mybookie.main), threading.Thread(target=xbet.main),
+               threading.Thread(target=youwager.main)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    print(datetime.datetime.now(), "Fetching live data finished!")
 
 
 def processOdds():
@@ -52,6 +136,7 @@ def processOdds():
     with open("result.json", "w") as rfile:
         json.dump(result, rfile, indent=4)
 
+
 def processFiles():
     data = {}
     for directory in os.listdir('./'):
@@ -71,27 +156,29 @@ def processFiles():
                                 teams[team1] = team2
                             del league['Team1']['Name']
                             del league['Team2']['Name']
+                            league['Team1']['Sport'] = directory
+                            league['Team2']['Sport'] = directory
                             games[f][team1] = league['Team1']
                             games[f][team2] = league['Team2']
             # print("============================================")
-            # print(json.dumps(teams, indent=4))
+            # print("Teams", json.dumps(teams, indent=4))
             # print("============================================")
-            # print(json.dumps(games, indent=4))
+            # print("Games", json.dumps(games, indent=4))
 
             for smt in ["Spread", "Money", "Total"]:
                 if smt not in data.keys():
                     data[smt] = {}
                 for team in teams.keys():
                     # if team not in data[smt].keys():
-                    data[smt][team] = {"Value": "0", "Service": "None"}
+                    data[smt][team] = {"Value": "0", "Service": "None","Sport":""}
                     # break
                     # if teams[team] not in data[smt].keys():
-                    data[smt][teams[team]] = {"Value": "0", "Service": "None"}
+                    data[smt][teams[team]] = {"Value": "0", "Service": "None","Sport":""}
                     # break
             # print("=============================================")
             # print("Data: ", json.dumps(data, indent=4))
 
-            for service in games.keys():
+            for service in [k for k in games.keys()][:-1]:
                 for team in teams.keys():
                     for smt in ["Spread", "Money", "Total"]:
                         if team in games[service].keys():
@@ -99,13 +186,22 @@ def processFiles():
                             # print(team, games[service][team][smt])
                             if data[smt][teams[team]]['Service'] != service and float(
                                     data[smt][team]['Value'].split()[-1]) < float(
-                                    games[service][team][smt].split()[-1]):
-                                data[smt][team] = {"Value": games[service][team][smt], "Service": service}
+                                games[service][team][smt].split()[-1]):
+                                data[smt][team] = {"Value": games[service][team][smt], "Service": service,
+                                                   "Sport": games[service][team]['Sport']}
                         if teams[team] in games[service].keys():
                             if data[smt][team]['Service'] != service and float(
                                     data[smt][teams[team]]['Value'].split()[-1]) < float(
-                                    games[service][teams[team]][smt].split()[-1]):
-                                data[smt][teams[team]] = {"Value": games[service][teams[team]][smt], "Service": service}
+                                games[service][teams[team]][smt].split()[-1]):
+                                try:
+                                    data[smt][teams[team]] = {"Value": games[service][teams[team]][smt],
+                                                              "Service": service,
+                                                              "Sport": games[service][teams[team]]['Sport']}
+                                except:
+                                    traceback.print_exc()
+                                    # print("Error...")
+                                    # print(service, team)
+                                    # input(json.dumps(games, indent=4))
             # print("=============================================")
     # print(json.dumps(data, indent=4))
     with open("highest.json", 'w') as hfile:
@@ -116,11 +212,15 @@ def createMap():
     teams = []
     teammap = {}
     for directory in os.listdir():
-        if os.path.isdir(directory) and not directory.startswith('.'):
+        if os.path.isdir(directory) and not directory.startswith('.') and not directory.startswith('_'):
             for file in os.listdir(directory):
                 if not file.startswith("-"):
-                    with open(f"./{directory}/{file}") as infile:
-                        js = json.load(infile)
+                    with open(f"./{directory}/{file}", encoding='utf8') as infile:
+                        try:
+                            js = json.load(infile)
+                        except:
+                            print(directory, file)
+                            traceback.print_exc()
                         try:
                             leagues = js[[k for k in js.keys()][0]]
                             for league in leagues:
@@ -191,11 +291,11 @@ def createMap():
 
 def convertOdds():
     for directory in os.listdir('./'):
-        if os.path.isdir(directory) and not directory.startswith('.'):
+        if os.path.isdir(directory) and not directory.startswith('.') and not directory.startswith('_'):
             for file in os.listdir(directory):
                 if not file.startswith("-"):
                     newjs = []
-                    with open(f'./{directory}/{file}') as jfile:
+                    with open(f'./{directory}/{file}', encoding='utf8') as jfile:
                         js = json.load(jfile)
                         # print(json.dumps(js, indent=4))
                         key = [k for k in js.keys()][0]
@@ -247,5 +347,15 @@ def getres(res=""):
     return res
 
 
+def apprun():
+    app.run(host="0.0.0.0", port=80)
+
+
 if __name__ == '__main__':
-    main()
+    threading.Thread(target=apprun).start()
+    while True:
+        main()
+        x = 10000
+        print(datetime.datetime.now(), f"Waiting for {x} seconds...")
+        time.sleep(x)
+        print(datetime.datetime.now(), "Wait finished, starting again!")
